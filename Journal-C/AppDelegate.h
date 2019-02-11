3c57a27e2257d31c0ff349dd1e2796d5806c3fb9
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-11 13:27:19 -0700

    initial commit

---
+++ Journal-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Journal-C
+//
+//  Created by Dominic Lanzillotta on 2/11/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
